ea5b049bfe3b413383bf314c67f6a9207cc54f2e
Lanzhou Jiang <lzjiang99@gmail.com>
2020-08-26 22:25:59 +1000

    Create User Class, write constructors

--- Magic/Magic.cs
+++ Magic/Magic.cs
@@ -3,19 +3,54 @@
 namespace Magic.Year
 {
  
-    public class MagicYear
+    public class MagicYearCalculator
     {
 
-        public bool IsMagic(int candidate)
+        public static void WelcomeUser()
         {
-            throw new NotImplementedException("Not implemented.");
+            Console.WriteLine("Welcome to the magic year calculator!");
         }
 
 
         public static void Main()
         {
-         
-            Console.WriteLine("Hello World");
+            WelcomeUser();
+            User newUser = new User();
+            Console.WriteLine(newUser.Name);
         }
+    }
+
+    public class User
+    {
+      private string name;
+      public string Name{
+        get {return name;}
+        set {name = value;}
+      }
+      private string surname;
+      public string Surname{
+        get {return surname;}
+        set {surname = value;}
+      }
+      private int workStartYear; 
+      public int WorkStartYear{
+        get {return workStartYear;}
+        set {workStartYear = value;}
+      }
+
+      public User(string userName, string userSurname, int userWorkStartYear){
+        name = userName;
+        surname = userSurname;
+        workStartYear = userWorkStartYear;
+      }
+
+      public User(){
+        Console.WriteLine("Please input your name: ");
+        name = Console.ReadLine();
+        Console.WriteLine("Please input your surname: ");
+        surname = Console.ReadLine();
+        Console.WriteLine("Please input your work start year: ");
+        workStartYear = int.Parse(Console.ReadLine());
+      }
     }
 }
