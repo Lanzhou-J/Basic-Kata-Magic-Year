bc2bc155d8ea9c1a0a11bb4b1ab35bc29fe0ec61
Lanzhou Jiang <lzjiang99@gmail.com>
2020-09-01 14:13:01 +1000

    add exception handling in Main function

--- Magic/CalculatorApp.cs
+++ Magic/CalculatorApp.cs
@@ -15,20 +15,35 @@ namespace Magic.Year
         public static void Main()
         {
             WelcomeUser();
-            UserInput newUserInput = new UserInput(); 
-            User newUser = newUserInput.InstantiateNewUser();
-            Salary annualSalary = newUserInput.InstantiateAnnualSalary();
-            Work newWork = newUserInput.InstantiateNewWork(annualSalary);
+            UserInput newUserInput = new UserInput();
 
-            var fullname = newUser.FormatFullname();
+            var adding = true;
+            while (adding)
+            {
+                try
+                {
+                    User newUser = newUserInput.InstantiateNewUser();
+                    Salary annualSalary = newUserInput.InstantiateAnnualSalary();
+                    Work newWork = newUserInput.InstantiateNewWork(annualSalary);
+                
+                    var fullname = newUser.FormatFullname();
             
-            Calculator newCalculator = new Calculator();
-            var magicYear = newCalculator.CalculateMagicYear(newWork.StartYear);
+                    Calculator newCalculator = new Calculator();
+                    var magicYear = newCalculator.CalculateMagicYear(newWork.StartYear);
 
-            var monthlySalary = newWork.CalculateMonthlySalary().RoundedAmount;
+                    var monthlySalary = newWork.CalculateMonthlySalary().RoundedAmount;
             
-            EmployeeReport newReport = new EmployeeReport(fullname:fullname, magicYear:magicYear, monthlySalary:monthlySalary);
-            newReport.PrintReport();
+                    EmployeeReport newReport = new EmployeeReport(fullname:fullname, magicYear:magicYear, monthlySalary:monthlySalary);
+                    newReport.PrintReport();
+
+                    adding = false;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Please try again");
+                }
+            }
         }
     }
 
